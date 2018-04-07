2aaa134c4c2462080aac0dbdac8b384360086fd6
coinop-logan <coinop.logan@gmail.com>
2018-04-07 04:30:44 +0000

    Demo shows larger pictures, better until lines are anti-aliased.

--- demo.py
+++ demo.py
@@ -19,25 +19,25 @@ def main():
             makeNewAddresses = False
             addresses = []
             icons = []
-            for i in range(100):
+            for i in range(16):
                 addresses.append(makeRandomEthAddress())
-                icons.append(identicon.generateIdenticon(addresses[-1], 50, 50))
+                icons.append(identicon.generateIdenticon(addresses[-1], 200, 200))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return
                 if event.key == pygame.K_SPACE:
                     #generate a dummy address
                     makeNewAddresses = True
-    
+
         screen.fill([0,0,0])
-        for i in range(100):
-            x = i%10
-            y = (i-x)/10
-            pos = [x*75 + 25, y*75 + 25]
+        for i in range(16):
+            x = i%4
+            y = (i-x)/4
+            pos = [x*225 + 25, y*225 + 25]
             screen.blit(icons[i], pos)
         pygame.display.flip()
 
